f629e3ea9ef75ccf7679e3688f15ada03e1d1875
boug <mustapha.bouglala@capgemini.com>
2020-03-02 12:15:11 +0100

    first commit

---
+++ movieprojet/src/main/java/com/capgmini/movieprojet/MovieprojetApplication.java
@@ -0,0 +1,13 @@
+package com.capgmini.movieprojet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieprojetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieprojetApplication.class, args);
+	}
+
+}
---
+++ movieprojet/src/test/java/com/capgmini/movieprojet/MovieprojetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgmini.movieprojet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieprojetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
