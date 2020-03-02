f7317b50053fdb8f3ce756dfb629769e5c410d97
boug <mustapha.bouglala@capgemini.com>
2020-03-02 17:31:58 +0100

    trie des movies

--- movieprojet/src/main/java/com/capgmini/movieprojet/repositories/MovieRepository.java
+++ movieprojet/src/main/java/com/capgmini/movieprojet/repositories/MovieRepository.java
@@ -1,9 +1,19 @@
 package com.capgmini.movieprojet.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.capgmini.movieprojet.models.Movie;
 
 public interface MovieRepository extends PagingAndSortingRepository<Movie, Integer>{
+	
+	public Page<Movie> findByOrderByTitleAsc(Pageable page);
+	
+	@Query(value = "select m from Movie as m where duration >= 120")
+	public Page<Movie> findMovieDuration(Pageable page);
+	
+	public Page<Movie> findByDurationGreaterThanEqual(Pageable page,int duree);
 
 }
--- movieprojet/src/main/java/com/capgmini/movieprojet/web/MovieController.java
+++ movieprojet/src/main/java/com/capgmini/movieprojet/web/MovieController.java
@@ -7,6 +7,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.capgmini.movieprojet.models.Movie;
@@ -22,7 +23,26 @@ public class MovieController {
 	
 	
 	@GetMapping
-	public Page<Movie>  listMovies (@PageableDefault(page = 0, size = 8) Pageable page){
+	public Page<Movie> listMovies (@PageableDefault(page = 0, size = 8) Pageable page){
 		return this.movieRepository.findAll(page);
 	}
+	
+	
+	@GetMapping("/AZ")
+	public Page<Movie> listMoviesAZ (@PageableDefault(page = 0, size = 8) Pageable page){
+		return this.movieRepository.findByOrderByTitleAsc(page);
+		}
+	
+	@GetMapping("/duration")
+	public Page<Movie> listMoviesDuration (@PageableDefault(page = 0, size = 8) Pageable page){
+		return this.movieRepository.findMovieDuration(page);
+		}
+	
+	@GetMapping("/duree")
+	public Page<Movie> listMoviesDuree (@PageableDefault(page = 0, size = 8) Pageable page,@RequestParam("duree")int duree){
+		return this.movieRepository.findByDurationGreaterThanEqual(page, duree);
+		}
+	
+	
+	
 }
